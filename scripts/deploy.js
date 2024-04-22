9d4a3b1aac3088f42902178e6cfbf4d9a85aeec3
mallikarjunreddyD <arjun753016@gmail.com>
2024-04-22 08:20:57 +0530

    Add transfer

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,12 +2,15 @@ const hre = require("hardhat")
 
 async function main() {
     const [deployer] = await ethers.getSigners()
-
     const SmallBank = await ethers.getContractFactory("smallBank")
     const smallBank = await SmallBank.deploy();
     await smallBank.deployed()
 
     console.log(`contract deployed at ${smallBank.address}`);
+    const transaction = await smallBank.connect(deployer).transferFromBank(deployer.address,1000)
+    const receipt = await transaction.wait()
+    const balance = await smallBank.connect(deployer).balanceOf(deployer.address);
+    console.log(` Address ${deployer.address}  balance is ${balance}`)
 }
 
 main().catch((error)=> {
